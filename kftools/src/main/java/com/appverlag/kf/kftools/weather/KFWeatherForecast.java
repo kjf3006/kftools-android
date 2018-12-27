c75d5265e5d853e6ab5d9f5cb3a5cb4e54439bd0
kevinflachsmann <kevin.flachsmann@icloud.com>
2018-12-27 17:42:53 +0100

    1.7.4.1

---
+++ kftools/src/main/java/com/appverlag/kf/kftools/weather/KFWeatherForecast.java
@@ -0,0 +1,10 @@
+package com.appverlag.kf.kftools.weather;
+
+/**
+ * Copyright (C) Kevin Flachsmann - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Created by kevinflachsmann on 23.12.18.
+ */
+public class KFWeatherForecast {
+}
